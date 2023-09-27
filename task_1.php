7d2921ada3e2e5a0a04e188c343f772ef992e5a7
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-09-28 00:10:20 +0600

    task1

--- task_1.php
+++ task_1.php
@@ -1,7 +1,13 @@
 <?php
 
 $text = "The brown fox jumps over the lazy dog";
+$small = strtolower($text);
 
+function manipulate($small){
+    $result = str_replace("brown", "red", $small);
+    echo $result;
+}
+manipulate($small)
 
 
 ?>
